98690065a37661362d960f70a9f3a3407b434d9c
michele <michele.bernardino@ufv.br>
2022-04-10 18:08:00 -0300

    Created README.md file

---
+++ README.md
@@ -0,0 +1,26 @@
+<h2>Person API REST with Spring Boot</h2>
+
+API developed during live coding realized by digital innovation one.
+
+<h3>Technologies used</h3>
+This project was developed with:
+* Java 11
+* Spring Boot 2.6.6
+* Spring JPA
+* H2 database 1.4.1
+* Maven 3.6.3
+* Mapstruct 1.4.2
+* Lombok
+* Heroku
+
+<h3>Run</h3>
+```shell script
+mvn spring-boot:run 
+```
+By default, the API will be available at:
+```
+http://localhost:8080/api/v1/people
+```
+<h3>Changes and evolutions</h3>
+in progress
+
--- src/main/java/com/dio/personapi/service/PersonService.java
+++ src/main/java/com/dio/personapi/service/PersonService.java
@@ -6,6 +6,7 @@ import com.dio.personapi.entities.Person;
 import com.dio.personapi.exception.PersonNotFoundException;
 import com.dio.personapi.mapper.PersonMapper;
 import com.dio.personapi.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +14,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
+
     private final PersonRepository personRepository;
     private final PersonMapper personMapper;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository, PersonMapper personMapper) {
-        this.personRepository = personRepository;
-        this.personMapper = personMapper;
-    }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
