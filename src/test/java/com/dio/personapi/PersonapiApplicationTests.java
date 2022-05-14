496ede51859da79497b89f615dfac0ff3c6ea4cf
michele <michele.bernardino@ufv.br>
2022-05-14 18:41:43 -0300

    test: Added Integration Tests with TestRestTemplate

--- src/test/java/com/dio/personapi/PersonapiApplicationTests.java
+++ src/test/java/com/dio/personapi/PersonapiApplicationTests.java
@@ -3,10 +3,10 @@ package com.dio.personapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PersonapiApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
