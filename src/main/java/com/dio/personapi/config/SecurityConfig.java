947583ffa0c386df5577d1bb9fbdc575c4e04b86
michele <michele.bernardino@ufv.br>
2022-05-16 12:34:25 -0300

    feat: Added spring security

---
+++ src/main/java/com/dio/personapi/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.dio.personapi.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+//                csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("Michele")
+                .password(passwordEncoder.encode("projectSpringBoot"))
+                .roles("USER", "ADMIN")
+                .and()
+                .withUser("Visit")
+                .password(passwordEncoder.encode("projectSpringBoot"))
+                .roles("USER");
+    }
+}
